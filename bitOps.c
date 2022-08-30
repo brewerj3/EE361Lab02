0ff3f6e737096b9e36ac37ac9bcd3321c843ae8a
brewerj3 <brewerj3@hawaii.edu.com>
2022-08-30 10:48:15 -1000

    Fixed bitOps.c

--- bitOps.c
+++ bitOps.c
@@ -12,7 +12,7 @@ char updateBit(char c, int k, int bitIs1);
 
 void main()
 {
-char value = 0xab;
+char value = 0xbf;
 printf("value =");
 displayBits(value);
 printf("\n");
@@ -34,7 +34,7 @@ position = 1;
 printf("setBit:  Bit %d; Result = ", position);
 displayBits(setBit(value,position));
 printf("\n");
-position = 2;
+position = 6;
 printf("setBit:  Bit %d; Result = ", position);
 displayBits(setBit(value,position));
 printf("\n");
@@ -82,49 +82,46 @@ displayBits(updateBit(value,position,bitIs1));
 printf("\n");
 }
 
-char getBit(char c, int k)
-{
-return '0' + ((c & (1 << k))!=0);
+char getBit(char c, int k) {
+    return '0' + ((c & (1 << k))!=0);
 }
 
-char getBit2(char c, int k)
-{
-char result = c >> k;      /* Shift the bit to position 0 */
-result = result & 1;       /* Mask the bits except bit 0 */
-return '0' + result;       /* Return the bit as a char */
+char getBit2(char c, int k) {
+    char result = c >> k;      /* Shift the bit to position 0 */
+    result = result & 1;       /* Mask the bits except bit 0 */
+    return '0' + result;       /* Return the bit as a char */
 }
 
-char setBit(char c, int k)
-{
-return c | (1 << k);
+char setBit(char c, int k) {
+    return c | (1 << k);
 }
 
-char clearBit(char c, int k)
-{
-return 0; 
+char clearBit(char c, int k) {
+    return c & ~(1 << k);
 }
 	
-char clearMSBitsThroughK(char c, int k)
-{
-char mask = (1<<k)-1;
-return c & mask;
+char clearMSBitsThroughK(char c, int k) {
+    char mask = (1 << k)-1;
+    return c & mask;
 }
 
-char clearLSBitsThroughK(char c, int k)
-{
-return 0;
+char clearLSBitsThroughK(char c, int k) {
+    char mask = ~((1 << (k+1))-1);
+    //printf("mask:%d",mask);
+    return c & mask;
 }
 
-char updateBit(char c, int k, int bitIs1)
-{
-return 0;
+char updateBit(char c, int k, int bitIs1) {
+    if(bitIs1 == 0 ){
+        return c & ~(1 << k);
+    }
+    return c & ~(1 << k);
 }
 
-void displayBits(char c)
-{
-for (int i=0; i<8; i++) {
-    printf("%c",'0'+((c>>(7-i))&1));
-}
+void displayBits(char c) {
+    for (int i=0; i<8; i++) {
+        printf("%c",'0'+((c>>(7-i))&1));
+    }
 }
 
 
--- mult.c
+++ mult.c
@@ -1,68 +1,63 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-unsigned int mult(unsigned int x, unsigned int y)
-{
-unsigned result = 0;
-unsigned int multiplicand = x;
-unsigned int multiplier = y;
+unsigned int mult(unsigned int x, unsigned int y) {
+    unsigned result = 0;
+    unsigned int multiplicand = x;
+    unsigned int multiplier = y;
 
-for (int i=0; i<32; i++) {
-    if (multiplier&1 == 1) {
-        result = result + multiplicand;
+    for (int i=0; i<32; i++) {
+        if (multiplier&1 == 1) {
+            result = result + multiplicand;
+        }
+        multiplier = multiplier >> 1;
+        multiplicand = multiplicand << 1;
     }
-    multiplier = multiplier >> 1;
-    multiplicand = multiplicand << 1;
+    return result;
+}
+
+unsigned int mult2(unsigned int x, unsigned int y) {
+    unsigned result = 0;
+    unsigned int multiplicand = x;
+    unsigned int multiplier = y;
+
+    for (int i=0; i<32; i++) {
+        if (multiplier&1 == 1) {
+            result = result + multiplicand;
+        }
+        multiplier = multiplier >> 1;
+        multiplicand = multiplicand << 1;
 }
 return result;
 }
 
-unsigned int mult2(unsigned int x, unsigned int y)
-{
-unsigned result = 0;
-unsigned int multiplicand = x;
-unsigned int multiplier = y;
-
-for (int i=0; i<32; i++) {
-    if (multiplier&1 == 1) {
-        result = result + multiplicand;
+unsigned int powMod(unsigned int x, unsigned int p, unsigned int m) {
+    unsigned int result = 1;
+    for (int i=0; i<p; i++) {
+        result = (result * x) % m;
     }
-    multiplier = multiplier >> 1;
-    multiplicand = multiplicand << 1;
-}
-return result;
+    return result;
 }
 
-unsigned int powMod(unsigned int x, unsigned int p, unsigned int m)
-{
-unsigned int result = 1;
-for (int i=0; i<p; i++) {
-    result = (result * x) % m;
-}
-return result;
-}
-
-unsigned int powMod2(unsigned int x, unsigned int p, unsigned int m)
-{
-unsigned int result = 1;
-for (int i=0; i<p; i++) {
-    result = (result * x) % m;
-}
-return result;
+unsigned int powMod2(unsigned int x, unsigned int p, unsigned int m) {
+    unsigned int result = 1;
+    for (int i=0; i<p; i++) {
+        result = (result * x) % m;
+    }
+    return result;
 }
 
-void main()
-{
-unsigned int i=123;
-unsigned int j=456;
-printf("%dx%d=%d: mult(%d,%d)=%d\n",i,j,i*j,i,j,mult(i,j));
-printf("%dx%d=%d: mult2(%d,%d)=%d\n",i,j,i*j,i,j,mult2(i,j));
+void main() {
+    unsigned int i=123;
+    unsigned int j=456;
+    printf("%dx%d=%d: mult(%d,%d)=%d\n",i,j,i*j,i,j,mult(i,j));
+    printf("%dx%d=%d: mult2(%d,%d)=%d\n",i,j,i*j,i,j,mult2(i,j));
 
-unsigned int x=3;
-unsigned int p=100000000;
-unsigned int m=53;
-printf("%d^%d mod %d: powMod = %d\n", x,p,m,powMod(x,p,m));
-printf("%d^%d mod %d: powMod2 = %d\n", x,p,m,powMod2(x,p,m));
+    unsigned int x=3;
+    unsigned int p=100000000;
+    unsigned int m=53;
+    printf("%d^%d mod %d: powMod = %d\n", x,p,m,powMod(x,p,m));
+    printf("%d^%d mod %d: powMod2 = %d\n", x,p,m,powMod2(x,p,m));
 }
 
 
