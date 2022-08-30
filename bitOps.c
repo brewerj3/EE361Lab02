62438dd5d8da19a5a0f4dda25f94e621e982e33e
brewerj3 <100174522+brewerj3@users.noreply.github.com>
2022-08-30 09:19:29 -1000

    added C files

---
+++ bitOps.c
@@ -0,0 +1,130 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void displayBits(char c);
+char getBit(char c, int k);
+char getBit2(char c, int k);
+char setBit(char c, int k);
+char clearBit(char c, int k);
+char clearMSBitsThroughK(char c, int k);
+char clearLSBitsThroughK(char c, int k);
+char updateBit(char c, int k, int bitIs1);
+
+void main()
+{
+char value = 0xab;
+printf("value =");
+displayBits(value);
+printf("\n");
+printf("\n");
+
+int position = 1;
+printf("getBit:  Bit %d = %c\n", position, getBit(value,position));
+position = 2;
+printf("getBit:  Bit %d = %c\n", position, getBit(value,position));
+printf("\n");
+
+position = 1;
+printf("getBit2:  Bit %d = %c\n", position, getBit2(value,position));
+position = 2;
+printf("getBit2:  Bit %d = %c\n", position, getBit2(value,position));
+printf("\n");
+
+position = 1;
+printf("setBit:  Bit %d; Result = ", position);
+displayBits(setBit(value,position));
+printf("\n");
+position = 2;
+printf("setBit:  Bit %d; Result = ", position);
+displayBits(setBit(value,position));
+printf("\n");
+printf("\n");
+
+position = 1;
+printf("clearBit:  Bit %d; Result = ", position);
+displayBits(clearBit(value,position));
+printf("\n");
+position = 2;
+printf("clearBit:  Bit %d; Result = ", position);
+displayBits(clearBit(value,position));
+printf("\n");
+printf("\n");
+
+position = 3;
+printf("clearMSBitsThroughK:  Bit %d, Result = ", position);
+displayBits(clearMSBitsThroughK(value,position));
+printf("\n");
+position = 1;
+printf("clearMSBitsThroughK:  Bit %d, Result = ", position);
+displayBits(clearMSBitsThroughK(value,position));
+printf("\n");
+printf("\n");
+
+position = 3;
+printf("clearLSBitsThroughK:  Bit %d, Result = ", position);
+displayBits(clearLSBitsThroughK(value,position));
+printf("\n");
+position = 5;
+printf("clearLSBitsThroughK:  Bit %d, Result = ", position);
+displayBits(clearLSBitsThroughK(value,position));
+printf("\n");
+printf("\n");
+
+position = 1;
+int bitIs1 = 0;
+printf("updateBit:  Bit %d, bitIs1=%d; Result = ", position,bitIs1);
+displayBits(updateBit(value,position,bitIs1));
+printf("\n");
+position = 2;
+bitIs1 = 1;
+printf("updateBit:  Bit %d; bitIs1=%d, Result = ", position,bitIs1);
+displayBits(updateBit(value,position,bitIs1));
+printf("\n");
+}
+
+char getBit(char c, int k)
+{
+return '0' + ((c & (1 << k))!=0);
+}
+
+char getBit2(char c, int k)
+{
+char result = c >> k;      /* Shift the bit to position 0 */
+result = result & 1;       /* Mask the bits except bit 0 */
+return '0' + result;       /* Return the bit as a char */
+}
+
+char setBit(char c, int k)
+{
+return c | (1 << k);
+}
+
+char clearBit(char c, int k)
+{
+return 0; 
+}
+	
+char clearMSBitsThroughK(char c, int k)
+{
+char mask = (1<<k)-1;
+return c & mask;
+}
+
+char clearLSBitsThroughK(char c, int k)
+{
+return 0;
+}
+
+char updateBit(char c, int k, int bitIs1)
+{
+return 0;
+}
+
+void displayBits(char c)
+{
+for (int i=0; i<8; i++) {
+    printf("%c",'0'+((c>>(7-i))&1));
+}
+}
+
+
---
+++ punyC.c
@@ -0,0 +1,207 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/* Opcodes */
+#define ADD  0
+#define DEC  1
+#define SET  2
+#define JMP  3
+#define JZ   4
+#define JNZ  5
+#define CALL 6
+#define RET  7
+
+/* CPU data registers */
+#define R0 0
+#define R1 1
+
+/* CPU registers */
+int r[2];
+int pc;
+int ir;
+int ra;
+
+/* Instruction memory */
+#define PROG_MEM_SIZE 16
+char instr[PROG_MEM_SIZE];
+
+/* Masks */
+#define MASK_8BITS 255
+#define MASK_4BITS 15 
+
+void load_prog1();
+void load_prog2();
+void run_prog();
+
+void main()
+{
+printf("Program 1:\n");
+load_prog1();  
+run_prog(20);  /* Simulate for 20 instructions */
+printf("\nProgram 2:\n");
+load_prog2();  
+run_prog(24);  
+}
+
+int get_bit(int n, int bit_pos)
+{
+return (n >> bit_pos) & 1;
+}
+
+int get_imm(int n)
+{
+return n & MASK_4BITS;
+}
+
+void instr_cycle()
+{
+int ir = MASK_8BITS & (int) (instr[pc]);
+pc++;
+int opcode = ir >> 5;
+switch(opcode) {
+    case ADD:
+	r[get_bit(ir,4)] = r[get_bit(ir,3)] + r[get_bit(ir,2)];
+        break;
+    case DEC:
+	r[get_bit(ir,4)] = r[get_bit(ir,4)] - get_imm(ir);
+        break;
+    case SET:
+	r[get_bit(ir,4)] = get_imm(ir);
+        break;
+    case JMP:
+	pc = get_imm(ir);
+        break;
+    case JZ:
+	if (r[get_bit(ir,4)] ==0 ) {
+		pc = get_imm(ir);
+	}
+        break;
+    case JNZ:
+	if (r[get_bit(ir,4)] !=0 ) {
+		pc = get_imm(ir);
+	}
+        break;
+    case CALL:
+        break;
+    case RET:
+        break;
+    default:
+	printf("Invalid instruction!\n");
+}
+}
+
+void run_prog(int sim_time)
+{
+printf("Simulation:\n");
+pc = 0; /* Initialize registers */
+r[0] = 0;
+r[1] = 0;
+/* Execute instructions */
+for (int t=0; t<sim_time; t++) {
+    printf("t=%2d: pc=%d, R0=%d, R1=%d\n",t,pc,r[0],r[1]);
+    instr_cycle();
+}
+}
+
+char set_opcode(int opcode)
+{
+return (char) (opcode << 5);
+}
+
+
+char set_bit(char c, int bit_val, int bit_pos)
+{
+if (bit_val==0) {
+    return c & ((char) (~(1 << bit_pos)));
+}
+else {
+    return c | ((char) (1 << bit_pos));
+}
+}
+
+char set_imm(char c, int imm)
+{
+char clear_bits = (char) (~15);
+return (c & clear_bits) | ((char) imm);
+}
+
+char instr_typeA(int opcode, int imm)
+{
+char i = set_opcode(opcode);
+i = set_imm(i, imm);
+return i;
+}
+
+char instr_typeB(int opcode, int dst, int imm)
+{
+char i = set_opcode(opcode);
+i = set_bit(i, dst, 4);
+i = set_imm(i, imm);
+return i;
+}
+
+char instr_typeC(int opcode, int dst, int src1, int src2)
+{
+char i = set_opcode(opcode);
+i = set_bit(i, dst, 4);
+i = set_bit(i, src1, 3);
+i = set_bit(i, src2, 2);
+return i;
+}
+
+void display_instr(char c)
+{
+int i = (int) c;
+i = i & MASK_8BITS;
+printf("opcode=%d, ", i>>5);
+printf("b[4]=%d, ", (i>>4)&1);
+printf("b[3..0]=%d%d%d%d, ", (i>>3)&1, (i>>2)&1, (i>>1)&1, i&1);
+}
+
+
+void display_prog()
+{
+printf("Program:\n");
+for (int i=0; i<PROG_MEM_SIZE; i++) {
+    printf("instr[%2d]: ",i);
+    display_instr(instr[i]);
+    printf("\n");
+}
+}
+
+void load_prog1()
+{
+for (int i=0; i<PROG_MEM_SIZE; i++) { /* Initialize program memory */
+    instr[i] = (char) 0;
+}
+
+instr[0] = instr_typeB(SET,R1,0);
+instr[1] = instr_typeB(SET,R0,3);
+instr[2] = instr_typeA(JZ,0);
+instr[3] = instr_typeC(ADD,R1,R1,R0);
+instr[4] = instr_typeB(DEC,R0,1);
+instr[5] = instr_typeA(JMP,2);
+
+display_prog();
+}
+
+void load_prog2()
+{
+for (int i=0; i<PROG_MEM_SIZE; i++) { /* Initialize program memory */
+    instr[i] = (char) 0;
+}
+
+instr[0] = instr_typeB(SET,R1,0);
+instr[1] = instr_typeB(SET,R0,2);
+instr[2] = instr_typeA(JZ,0);
+instr[3] = instr_typeA(CALL,7);
+instr[4] = instr_typeA(CALL,7);
+instr[5] = instr_typeB(DEC,R0,1);
+instr[6] = instr_typeA(JMP,2);
+instr[7] = instr_typeC(ADD,R1,R1,R0);
+instr[8] = instr_typeA(RET,0);
+
+display_prog();
+}
+
+
