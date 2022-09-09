efc72e8cf322d54610836b0525fdc46648e94d4c
brewerj3 <brewerj3@hawaii.edu.com>
2022-09-09 12:03:13 -1000

    added reverse_int.c

--- reverse_int.c
+++ reverse_int.c
@@ -1,8 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int reverse_int(int n)
-{
+int reverse_int(int n) {
+    int remainder = 0, reverse = 0;
+    while(n != 0) {
+        remainder = n % 10;
+        reverse = reverse * 10 + remainder;
+        n = n/10;
+    }
+    n = reverse;
     return n;
 }
 
